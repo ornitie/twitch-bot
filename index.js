c0a6fb639af8b81b3f2cce6e151210925a7a8708
Juan Pablo <uncorreodeinternet@gmail.com>
2021-08-10 17:03:51 -0500

    [#4] Added timer between iterations

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-(function () {
+(async function () {
   "use strict";
-  window.addEventListener("load", function () {
+  window.addEventListener("load", async function () {
     setTimeout(async function () {
       const RewardsButton = "Saldo de puntos";
 
+      for (var i = 0; i < 3; i++) {
+        function timer(ms) {
+          return new Promise((res) => setTimeout(res, ms));
+        }
+        console.log(i);
+        await timer(3000);
+      }
       function getButton() {
         return Array.from(document.getElementsByTagName("button")).find(
           (element) => element.ariaLabel === RewardsButton
