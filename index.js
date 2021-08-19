04fd09a79f1bac3497ecb4e7ab84ceea8ce28cce
Juan Pablo <uncorreodeinternet@gmail.com>
2021-08-18 21:31:35 -0500

    [#3] Added Wait until load

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 (function () {
   "use strict";
+  window.addEventListener("load", function () {
+    setTimeout(async function () {
+      const RewardsButton = "Saldo de puntos";
 
-  const RewardsButton = "Saldo de puntos";
+      function getButton() {
+        return Array.from(document.getElementsByTagName("button")).find(
+          (element) => element.ariaLabel === RewardsButton
+        );
+      }
 
-  function getButton() {
-    return Array.from(document.getElementsByTagName("button")).find(
-      (element) => element.ariaLabel === RewardsButton
-    );
-  }
+      function x() {
+        console.log(":V");
+      }
 
-  getButton().click();
+      getButton().click();
+      x();
+      console.log("YEAH");
+    }, 3000);
+  });
 })();
