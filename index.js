7dba1c69787c18f707c6e3943a34f5931e774d96
Juan Pablo <uncorreodeinternet@gmail.com>
2021-08-22 17:03:51 -0500

    Refactored into HTTP post

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
 (async function () {
   "use strict";
+
+  async function postData(url = "", data = {}, authorization = "") {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `OAuth ${authorization}`,
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  }
+
+  var data = [
+    {
+      operationName: "SendHighlightedChatMessage",
+      variables: {
+        input: {
+          channelID: "124422593",
+          cost: 800,
+          message: "KEKW",
+          transactionID: "aa71cbb5cd22e9165ah92a1525b03120",
+        },
+      },
+      extensions: {
+        persistedQuery: {
+          version: 1,
+          sha256Hash:
+            "bb187d763156dc5c25c6457e1b32da6c5033cb7504854e6d33a8b876d10444b6",
+        },
+      },
+    },
+  ];
+  var url = "https://gql.twitch.tv/gql";
+  var authToken = "024n0op1qbucpyk7rjg7g0mwqefyzn";
+
+  var response = await postData(url, data, authToken);
+  console.log(response);
+
   window.addEventListener("load", async function () {
     setTimeout(async function () {
-      const RewardsButton = "Saldo de puntos";
-
+      function setMessage() {
+        const textBox = Array.from(
+          document.getElementsByTagName("textArea")
+        )[0];
+        textBox.value = Math.floor(Math.random() * (999999 - 1)) + 1;
+      }
       for (var i = 0; i < 3; i++) {
         function timer(ms) {
           return new Promise((res) => setTimeout(res, ms));
         }
-        console.log(i);
+        getButton().click();
+        getGuessButton().click();
+        getSendButton().click();
+        setMessage();
+        console.log("poggy");
         await timer(3000);
       }
-      function getButton() {
-        return Array.from(document.getElementsByTagName("button")).find(
-          (element) => element.ariaLabel === RewardsButton
-        );
-      }
-
-      function getGuessButton() {
-        return Array.from(document.getElementsByTagName("p")).find(
-          (element) => element.innerHTML == "Guess a number 1 - 1 million"
-        );
-      }
-
-      function setMessage(message) {
-        const textBox = Array.from(document.getElementsByTagName("textArea"));
-        textBox.value = "KEKW";
-      }
-
-      function x() {
-        console.log(":V");
-      }
-
-      getButton().click();
-      getGuessButton().click();
-      setMessage();
-      x();
-      console.log("YEAH");
     }, 3000);
   });
 })();
